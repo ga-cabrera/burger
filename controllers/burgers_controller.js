a07f213539750cd9b4abd7c6248c8edd1b9e3a8f
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-10-31 14:50:55 -0700

    added context for main.handlebars and added logic file for jquery code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -42,3 +42,19 @@ app.put('/api/burgers/:id', function(req, res) {
         }
     });
 });
+
+app.delete('/api/burgers/:id', function(req, res) {
+    var condition = `id = ${req.params.id}`;
+
+    burger.delete(condition, function(result) {
+        if (result.affectedRows == 0) {
+            // if no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+// export routes for server.js to use
+module.exports = app;
