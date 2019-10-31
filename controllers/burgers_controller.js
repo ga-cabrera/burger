dddcf8b4af359d89c59aae0b3b5cfbd3ac2f900d
Gerson Cabrera <ga.cabrera05@gmail.com>
2019-10-30 19:59:51 -0700

    added /api/burgers/:id route function when for when we update a burger from devoured false to devoured true

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -25,3 +25,20 @@ app.post('/api/burgers', function(req, res) {
         res.json({ id: result.insertId });
     });
 });
+
+app.put('/api/burgers/:id', function(req, res) {
+    var condition = `id = ${req.params.id}`;
+
+    console.log('condition', condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            // if now rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
